3202382ae18cbce783bca44adf1378138b73ef3b
Joseph Laquinte <joseph.laquinte@ogilvy.com>
2019-01-24 04:11:32 -0500

    Switch static json data with HTTPRequest from custom endpoint

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,11 +1,27 @@
 /**
  * Mocking client-server processing
  */
-import _products from './products.json'
+import axios from 'axios'
 
 const TIMEOUT = 100
+const requestURL = 'http://tech.work.co/shopping-cart/products.json'
+
+/***
+Use axios for HTTP Request from API
+***/
+
+const getProductsFromEndpoint = (cb) =>{
+	axios.get(requestURL)
+	.then(response => {
+		console.log(response.data)
+		cb(response.data)
+	})
+	.catch(error => {console.log(`*** API ERROR *** ${error}`)})
+}
+
 
 export default {
-  getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
+  getProducts: getProductsFromEndpoint,
   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
 }
+
